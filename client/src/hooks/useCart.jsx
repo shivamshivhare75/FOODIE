8ce60c8b47544de7e0d5eb53c6b94ba7ec3ebf5d
Shivam Shivhare <shivamshivhare75@gmail.com>
2024-08-10 20:46:09 +0530

    update

--- client/src/hooks/useCart.jsx
+++ client/src/hooks/useCart.jsx
@@ -1,22 +1,12 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
-
 const useCart = () => {
   const { user } = useContext(AuthContext);
   const token = localStorage.getItem("access-token");
-
-  // Use useQuery only if user is logged in
-  const {
-    refetch,
-    data: cart = [],
-    isFetching,
-  } = useQuery({
+  const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
-      if (!user?.email) {
-        return []; // Return an empty array if no user is logged in
-      }
       const res = await fetch(
         `https://foodie-backend-78wt.onrender.com/carts?email=${user.email}`,
         {
@@ -25,15 +15,10 @@ const useCart = () => {
           },
         }
       );
-      if (!res.ok) {
-        throw new Error("Failed to fetch cart");
-      }
       return res.json();
     },
-    enabled: !!user?.email, // Only fetch if user is logged in
   });
-
-  return [cart, refetch, isFetching];
+  return [cart, refetch];
 };
 
 export default useCart;
